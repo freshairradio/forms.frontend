1b0c7ad4ac2fd1fedb65b7fb5c71cb68b47e6dc6
Samuel Macleod <samuel@macleod.space>
2020-01-21 04:53:09 +0000

    Add auth

--- src/pages/[slug].vue
+++ src/pages/[slug].vue
@@ -1,6 +1,8 @@
 <template>
   <Layout>
     <Form :spec="spec" v-if="spec" @sync="sync" :name="name" />
+    <h2 class="me" v-if="person_name">{{ person_name }}</h2>
+    <a class="problems" href="mailto:webmaster@freshair.org.uk">Need help?</a>
   </Layout>
 </template>
 
@@ -18,6 +20,26 @@ export default {
   },
   async mounted() {
     const { slug } = this.$route.params;
+
+    if (!localStorage.getItem("token")) {
+      window.location = `https://auth.freshair.org.uk/for/forms/${slug}`;
+      return;
+    }
+    let me = await fetch(`https://auth.api.freshair.org.uk/verify`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Auth-Token": localStorage.getItem("token")
+      }
+    }).then(r => r.json());
+    if (!me.ok) {
+      console.error("Huh?");
+      window.location = `http://auth.freshair.org.uk/force/forms/${slug}`;
+      return;
+    } else {
+      this.person_name = me.name;
+      console.log(me.name);
+    }
     let spec = await fetch(
       `https://forms.api.freshair.org.uk/spec/${slug}`
     ).then(r => r.json());
@@ -46,7 +68,8 @@ export default {
     return {
       slug: "",
       spec: null,
-      name: ""
+      name: "",
+      person_name: null
     };
   }
 };
@@ -63,5 +86,19 @@ h1.page-title {
   justify-items: center;
   align-items: center;
   height: 100vh;
+}
+.me {
+  position: fixed;
+  z-index: 90;
+  bottom: 10px;
+  left: 20px;
+  color: white;
+}
+.problems {
+  position: fixed;
+  z-index: 90;
+  bottom: 10px;
+  right: 20px;
+  color: white;
 }
 </style>
---
+++ src/pages/token/[token]/[spec].vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout> </Layout>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    const { token, spec } = this.$route.params;
+    localStorage.setItem("token", token);
+    this.$router.push(`/${spec}`);
+  },
+  metaInfo: {
+    title: "Token"
+  }
+};
+</script>
+<style scoped>
+.card {
+  max-width: 400px;
+  margin: 0 auto;
+}
+.circle {
+  margin-bottom: 40px;
+  margin-top: 100px;
+}
+.actions {
+  margin-top: 20px;
+}
+</style>
